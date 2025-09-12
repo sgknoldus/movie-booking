9eb4c97019bd74685ae3bafc3a005e7b7da3550f
Sushant Gupta <er.sushantg@gmail.com>
2025-09-12 21:50:06 +0530

    refactor: simplify BookingConfirmedEvent class by using Lombok annotations for boilerplate code reduction

--- common-lib/src/main/java/com/moviebooking/common/events/booking/BookingConfirmedEvent.java
+++ common-lib/src/main/java/com/moviebooking/common/events/booking/BookingConfirmedEvent.java
@@ -1,40 +1,31 @@
 package com.moviebooking.common.events.booking;
 
 import com.moviebooking.common.events.DomainEvent;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class BookingConfirmedEvent extends DomainEvent {
-    private final String bookingId;
-    private final Long userId;
-    private final Long showId;
-    private final Long theatreId;
-    private final Long movieId;
-    private final List<String> seatNumbers;
-    private final BigDecimal totalAmount;
-    private final String paymentId;
-    private final LocalDateTime showDateTime;
-    private final LocalDateTime confirmedAt;
-    private final String eventType;
-
-    public BookingConfirmedEvent(String bookingId, Long userId, Long showId, Long theatreId,
-                                 Long movieId, List<String> seatNumbers, BigDecimal totalAmount,
-                                 String paymentId, LocalDateTime showDateTime, LocalDateTime confirmedAt) {
-        super();
-        this.bookingId = bookingId;
-        this.userId = userId;
-        this.showId = showId;
-        this.theatreId = theatreId;
-        this.movieId = movieId;
-        this.seatNumbers = seatNumbers;
-        this.totalAmount = totalAmount;
-        this.paymentId = paymentId;
-        this.showDateTime = showDateTime;
-        this.confirmedAt = confirmedAt;
-        this.eventType = "BOOKING_CONFIRMED";
-    }
+    private String bookingId;
+    private Long userId;
+    private Long showId;
+    private Long theatreId;
+    private Long movieId;
+    private List<String> seatNumbers;
+    private BigDecimal totalAmount;
+    private String paymentId;
+    private LocalDateTime showDateTime;
+    private LocalDateTime confirmedAt;
+    private String eventType = "BOOKING_CONFIRMED";
 }
